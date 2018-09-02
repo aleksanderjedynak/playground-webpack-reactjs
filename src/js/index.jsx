91fc0cf8863bb5fafde09a1c81e17a151224d764
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-09-02 23:33:04 +0200

    add index

---
+++ src/js/index.jsx
@@ -0,0 +1,9 @@
+import styles from"../sass/main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "es6-promise/auto";
+
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("app"));
